9b11bc8cd7475e702548789a91ca3c05a1f80be4
Stijn Lingmont <stijnlingmont@hotmail.com>
2020-10-18 16:42:44 +0200

    Helper function

--- command.js
+++ command.js
@@ -1,11 +1,13 @@
 const { prefix } = require("./config.json")
 
 module.exports = (client, aliases, callback) => {
-    if(typeof(aliases === 'string'))
+    if(typeof(aliases) === 'string')
         aliases = [aliases]
   
     client.on('message', message => {
         const { content } = message
+
+        console.log(aliases)
 
         aliases.forEach(alias => {
             const command = `${prefix}${alias}`
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
 const config = require("./config.json")
+const commandList = require("./commands.json")
 
 const command = require('./command')
 
 client.on('ready', () => {
     console.log('The client is ready!')
 
-    command(client, 'ping', message => {
-        message.channel.send('Pong!')
+    command(client, commandList["serverList"].commands, message => {
+        client.guilds.cache.forEach(guild => {
+            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`)
+        })
+    })
+
+    command(client, commandList["help"].commands, message => {
+        var commands = [];
+        console.log(commandList)
+        for(var key in commandList) {
+            var command = commandList[key]
+            commands.push({
+                name: command.name,
+                value: command.commands,
+                inline: false
+            })
+        }
+
+        const helperList = new Discord.MessageEmbed()
+        .setTitle("List of all commands")
+        .addFields(commands)
+
+        message.channel.send(helperList)
     })
 })
 
